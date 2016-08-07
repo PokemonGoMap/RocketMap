fd80e7695f4c1d5af7b4e72ca9a8e138ed8ff15e
Nicholas Totsch <nicholas.totsch@gmail.com>
2016-08-07 17:00:52 -0500

    Added coords-only.sh as bandaid for location-Generator.py

---
+++ contrib/supervisord/coords-only.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Using this until they allow location_generator.py to allow coords only mode!
+
+output="coords.txt"   # Name of the coords file
+lat="29.882687"       # Latitiude
+lon="-97.94071"       # Longitude
+st="5"                # Steps
+lp="6"                # Leaps
+
+# Runs location_generator.py with info from above, outputs to a caca file after taking out
+# the top line "Generating yadayada" and places it neatly where we want supervisord to see it
+/usr/bin/python ../../Tools/Hex-Beehive-Generator/location_generator.py -lat $lat -lon $lon \
+-st $st -lp $lp -o caca -v | sed '/Generating/d' > ~/supervisor/$output
+
+# Get rid of caca beehive file
+rm caca
