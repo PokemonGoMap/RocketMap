9007ff74dd3db9aebf90831fdcd564e9b5b4b5e2
evgenij <evgenij.schwyndja@googlemail.com>
2016-08-08 23:54:18 +0200

    Revert "Revert "PokemapWatcher""
    This reverts commit 9d4a08ea0d0ffc9e209a6dfc789653149367cec5.

---
+++ Tools/PokemapWatcher/PokemapWatcher/Model/WorkerInstance.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PokemapWatcher.Model
+{
+    public class WorkerInstance : AbstractInstance, INotifyPropertyChanged
+    {
+        public WorkerInstance ()
+        {
+            InstanceName = "New Worker Instance";
+        }
+
+        #region Searcher Settings
+        [XmlIgnore]
+        AuthService m_AuthService = AuthService.ptc;
+        [XmlAttribute]
+        public AuthService AuthService
+        {
+            get { return m_AuthService; }
+            set
+            {
+                m_AuthService = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [XmlIgnore]
+        string m_Username = "";
+        [XmlAttribute]
+        public string Username
+        {
+            get { return m_Username; }
+            set
+            {
+                m_Username = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [XmlIgnore]
+        string m_Password = "";
+        [XmlAttribute]
+        public string Password
+        {
+            get { return m_Password; }
+            set
+            {
+                m_Password = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [XmlIgnore]
+        public UInt16 m_StepLimit = 5;
+        [XmlAttribute]
+        public UInt16 StepLimit
+        {
+            get { return m_StepLimit; }
+            set
+            {
+                m_StepLimit = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        [XmlIgnore]
+        public UInt16 m_StepDelay = 5;
+        [XmlAttribute]
+        public UInt16 StepDelay
+        {
+            get { return m_StepDelay; }
+            set
+            {
+                m_StepDelay = value;
+                NotifyPropertyChanged();
+            }
+        }
+        #endregion
+        
+        #region Methods
+        public override string BuildProcArguments()
+        {
+            string args = "";
+
+            args += "--no-server ";
+            if (AuthService == AuthService.ptc)
+                args += "-a ptc ";
+            else if (AuthService == AuthService.google)
+                args += "-a google ";
+            // Username
+            args += "-u " + Username + " ";
+            // Password
+            args += "-p " + Password + " ";
+            // Step Limit
+            args += "-st " + StepLimit + " ";
+            // Step Delay
+            args += "-sd " + StepDelay + " ";
+
+            return args;
+        }
+        public override void RefreshGMapsKey(string key)
+        {
+            return;
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        #endregion
+    }
+}
---
+++ Tools/PokemapWatcher/PokemapWatcher/ViewModel/MainViewVM.cs
@@ -0,0 +1,181 @@
+﻿using PokemapWatcher.Commands;
+using PokemapWatcher.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PokemapWatcher.ViewModel
+{
+    public class MainViewVM : INotifyPropertyChanged
+    {
+        public MainViewVM()
+        {
+            ServerList = new ObservableCollection<WebserverInstance>(SettingsProvider.readSettings());
+
+            #region Commands
+            CloseCommand = new RelayCommand(Close);
+
+            CreateServerCommand = new RelayCommand(CreateServer);
+            DeleteServerCommand = new RelayCommand(DeleteServer);
+
+            SaveSettingsCommand = new RelayCommand(SaveSettings);
+                                    
+            StartProcAllCommand = new RelayCommand(StartProcAll);
+            StopProcAllCommand = new RelayCommand(StopProcAll);
+            #endregion
+        }
+
+        #region Commands
+        private ICommand m_CloseCommand;
+        public ICommand CloseCommand
+        {
+            get { return m_CloseCommand; }
+            set
+            {
+                m_CloseCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void Close(object o)
+        {
+            StopProcAllCommand.Execute(null);
+        }
+
+
+        private ICommand m_CreateServerCommand;
+        public ICommand CreateServerCommand
+        {
+            get { return m_CreateServerCommand; }
+            set
+            {
+                m_CreateServerCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void CreateServer(object o)
+        {
+            ServerList.Add(new WebserverInstance());
+        }
+
+        private ICommand m_DeleteServerCommand;
+        public ICommand DeleteServerCommand
+        {
+            get { return m_DeleteServerCommand; }
+            set
+            {
+                m_DeleteServerCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void DeleteServer(object o)
+        {
+            if (SelectedServer != null)
+            {
+                var s = SelectedServer;
+                SelectedServer = null;
+                ServerList.Remove(s);
+            }
+        }
+        
+
+        private ICommand m_SaveSettingsCommand;
+        public ICommand SaveSettingsCommand
+        {
+            get { return m_SaveSettingsCommand; }
+            set
+            {
+                m_SaveSettingsCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void SaveSettings (object o)
+        {
+            SettingsProvider.writeSettings(new List<WebserverInstance>(ServerList));
+        }
+                
+
+        private ICommand m_StartProcAllCommand;
+        public ICommand StartProcAllCommand
+        {
+            get { return m_StartProcAllCommand; }
+            set
+            {
+                m_StartProcAllCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void StartProcAll(object o)
+        {
+            foreach (WebserverInstance ins in ServerList)
+            {
+                ins.Start();
+                foreach(WorkerInstance ins2 in ins.WorkerInstanceCollection)
+                {
+                    ins2.Start();
+                }
+            }
+        }
+
+        private ICommand m_StopProcAllCommand;
+        public ICommand StopProcAllCommand
+        {
+            get { return m_StopProcAllCommand; }
+            set
+            {
+                m_StopProcAllCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private void StopProcAll(object o)
+        {
+            foreach (WebserverInstance ins in ServerList)
+            {
+                ins.Stop();
+                foreach (WorkerInstance ins2 in ins.WorkerInstanceCollection)
+                {
+                    ins2.Stop();
+                }
+            }
+        }
+        #endregion
+
+        #region Member
+        private ObservableCollection<WebserverInstance> m_ServerList;        
+        public ObservableCollection<WebserverInstance> ServerList
+        {
+            get { return m_ServerList; }
+            set
+            {
+                m_ServerList = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private WebserverInstance m_SelectedServer;
+        public WebserverInstance SelectedServer
+        {
+            get { return m_SelectedServer; }
+            set
+            {
+                m_SelectedServer = value;
+                NotifyPropertyChanged();
+            }
+        }
+        
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        #endregion
+    }
+}
