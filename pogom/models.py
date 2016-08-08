4175af563128a2a492368b4fd9f064186285de19
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2016-08-08 23:23:05 +0200

    Update models.py
    Applied suggested changes

--- pogom/models.py
+++ pogom/models.py
@@ -298,8 +298,6 @@ def parse_map(map_dict, step_location):
         if config['parse_pokemon']:
             for p in cell.get('catchable_pokemons', []):
                 d_t = datetime.utcfromtimestamp(p['expiration_timestamp_ms'] / 1000.0)
-                printPokemon(p['pokemon_id'], p['latitude'],
-                             p['longitude'], d_t)
                 pokemons[p['encounter_id']] = {
                     'encounter_id': b64encode(str(p['encounter_id'])),
                     'spawnpoint_id': p['spawn_point_id'],
@@ -312,8 +310,6 @@ def parse_map(map_dict, step_location):
                 d_t = datetime.utcfromtimestamp(
                     (p['last_modified_timestamp_ms'] +
                      p['time_till_hidden_ms']) / 1000.0)
-                printPokemon(p['pokemon_data']['pokemon_id'], p['latitude'],
-                             p['longitude'], d_t)
                 pokemons[p['encounter_id']] = {
                     'encounter_id': b64encode(str(p['encounter_id'])),
                     'spawnpoint_id': p['spawn_point_id'],
@@ -322,20 +318,6 @@ def parse_map(map_dict, step_location):
                     'longitude': p['longitude'],
                     'disappear_time': d_t
                 }
-
-                webhook_data = {
-                    'encounter_id': b64encode(str(p['encounter_id'])),
-                    'spawnpoint_id': p['spawn_point_id'],
-                    'pokemon_id': p['pokemon_data']['pokemon_id'],
-                    'latitude': p['latitude'],
-                    'longitude': p['longitude'],
-                    'disappear_time': calendar.timegm(d_t.timetuple()),
-                    'last_modified_time': p['last_modified_timestamp_ms'],
-                    'time_until_hidden_ms': p['time_till_hidden_ms'],
-                    'is_lured': False
-                }
-
-                send_to_webhook('pokemon', webhook_data)
                 
         for f in cell.get('forts', []):
             if config['parse_pokestops'] and f.get('type') == 1:  # Pokestops
@@ -380,11 +362,22 @@ def parse_map(map_dict, step_location):
                     'last_modified': datetime.utcfromtimestamp(
                         f['last_modified_timestamp_ms'] / 1000.0),
                 }
-
+                
+    # Create Pokemon list for webhook
+    webhook_pokemon = []
+    for p in pokemons:
+            printPokemon(p['pokemon_id'], p['latitude'], 
+                             p['longitude'], p['disappear_time'])
+            p['is_lured'] = False
+            webhook_pokemon.append(p)
+    
+    # Send Pokemon List to webhook
+    send_to_webhook('pokemon', webhook_pokemon)
+    
     pokemons_upserted = 0
     pokestops_upserted = 0
     gyms_upserted = 0
-
+    
     if pokemons and config['parse_pokemon']:
         pokemons_upserted = len(pokemons)
         bulk_upsert(Pokemon, pokemons)
