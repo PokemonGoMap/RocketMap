09fe2e062e5cdac38341b65f31635d7b4686c075
taihun81 <taihun@gmail.com>
2016-08-11 12:47:26 -0400

    Update search.py
    Delay after initial login to avoid first search step resulting 0/0/0

--- pogom/search.py
+++ pogom/search.py
@@ -292,6 +292,9 @@ def check_login(args, account, api, position):
 
     log.debug('Login for account %s successful', account['username'])
 
+    # To avoid first search step resulting 0/0/0
+    time.sleep(args.scan_delay)
+
 
 def map_request(api, position):
     try:
