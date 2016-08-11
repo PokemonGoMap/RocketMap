01fb95388fa3efca783f89cc618e49a2973d2605
Yuya <Sunakujira1@users.noreply.github.com>
2016-08-11 17:11:42 -0400

    Fixed whitespace issue

--- pogom/search.py
+++ pogom/search.py
@@ -208,7 +208,6 @@ def search_worker_thread(args, account, search_items_queue, parse_lock, encrypti
                 # The loop to try very hard to scan this step
                 failed_total = 0
                 while True:
- 
                     # Get current time
                     loop_start_time = time.time()
 
