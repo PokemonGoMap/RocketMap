bcefceb385bdd3db7fd770c6a2a52293ec724e58
Daemonmatrix <neomatrix99@gmail.com>
2016-08-11 06:29:41 -0400

    Viper2k4's Suggestion
    Fixing spacing issues for linux test.

--- static/js/map.js
+++ static/js/map.js
@@ -1183,31 +1183,31 @@ function setupPokemonMarker (item, skipNotification, isBounceDisabled) {
   return marker
 }
 
-function getMarkerPath(item) {
-    var gymLevel;
-    if (item.gym_points >= 50000) {
-        gymLevel = 10;
-    } else if (item.gym_points >= 40000) {
-        gymLevel = 9;
-    } else if (item.gym_points >= 30000) {
-        gymLevel = 8;
-    } else if (item.gym_points >= 20000) {
-        gymLevel = 7;
-    } else if (item.gym_points >= 16000) {
-        gymLevel = 6;
-    } else if (item.gym_points >= 12000) {
-        gymLevel = 5;
-    } else if (item.gym_points >= 8000) {
-        gymLevel = 4;
-    } else if (item.gym_points >= 4000) {
-        gymLevel = 3;
-    } else if (item.gym_points >= 2000) {
-        gymLevel = 2;
-    } else {
-        gymLevel = 1;
-    }
+function getMarkerPath (item) {
+	var gymLevel;
+	if (item.gym_points >= 50000) {
+		gymLevel = 10;
+	} else if (item.gym_points >= 40000) {
+		gymLevel = 9;
+	} else if (item.gym_points >= 30000) {
+		gymLevel = 8;
+	} else if (item.gym_points >= 20000) {
+		gymLevel = 7;
+	} else if (item.gym_points >= 16000) {
+		gymLevel = 6;
+	} else if (item.gym_points >= 12000) {
+		gymLevel = 5;
+	} else if (item.gym_points >= 8000) {
+		gymLevel = 4;
+	} else if (item.gym_points >= 4000) {
+		gymLevel = 3;
+	} else if (item.gym_points >= 2000) {
+		gymLevel = 2;
+	} else {
+		gymLevel = 1;
+	}
 
-    return 'static/forts/' + gymTypes[item['team_id']] + '_' + gymLevel + '.png';
+	return 'static/forts/' + gymTypes[item['team_id']] + '_' + gymLevel + '.png';
 }
 
 function setupGymMarker (item) {
