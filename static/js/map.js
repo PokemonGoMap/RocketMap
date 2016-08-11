003d3efb12f95fa365a20e7cf468fa495f03831c
Daemonmatrix <neomatrix99@gmail.com>
2016-08-11 06:47:30 -0400

    Viper2k4's Suggestion fixing spacing, tidying up the code.

--- static/js/map.js
+++ static/js/map.js
@@ -1185,27 +1185,26 @@ function setupPokemonMarker (item, skipNotification, isBounceDisabled) {
 
 function getMarkerPath (item) {
 	var gymLevel;
-	if (item.gym_points >= 50000) {
+	if (item.gym_points >= 50000)
 		gymLevel = 10;
-	} else if (item.gym_points >= 40000) {
+	else if (item.gym_points >= 40000)
 		gymLevel = 9;
-	} else if (item.gym_points >= 30000) {
+	else if (item.gym_points >= 30000)
 		gymLevel = 8;
-	} else if (item.gym_points >= 20000) {
+	else if (item.gym_points >= 20000)
 		gymLevel = 7;
-	} else if (item.gym_points >= 16000) {
+	else if (item.gym_points >= 16000)
 		gymLevel = 6;
-	} else if (item.gym_points >= 12000) {
+	else if (item.gym_points >= 12000)
 		gymLevel = 5;
-	} else if (item.gym_points >= 8000) {
+	else if (item.gym_points >= 8000)
 		gymLevel = 4;
-	} else if (item.gym_points >= 4000) {
+	else if (item.gym_points >= 4000)
 		gymLevel = 3;
-	} else if (item.gym_points >= 2000) {
+	else if (item.gym_points >= 2000)
 		gymLevel = 2;
-	} else {
+	else
 		gymLevel = 1;
-	}
 
 	return 'static/forts/' + gymTypes[item['team_id']] + '_' + gymLevel + '.png';
 }
