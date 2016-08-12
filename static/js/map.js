15b1d9db7e0487d722b0568129486fed6d545f18
James <james@studio-xp.com>
2016-08-12 15:55:10 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1582,6 +1582,7 @@ function sendNotification (title, text, icon, lat, lng) {
       onClick: function () {
         window.focus()
         this.close()
+
         centerMap(lat, lng, 20)
       }
     })
