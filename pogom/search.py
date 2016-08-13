21773eafa714bf3d0a77c58e8775d786eaa1847d
LurkerLurks <LurkerLurks@FakeEmail.com>
2016-08-13 09:33:00 -0700

    Moved delay and added log msg per chuyskywalker

--- pogom/search.py
+++ pogom/search.py
@@ -164,7 +164,8 @@ def search_overseer_thread(args, new_location_queue, pause_bit, encryption_lib_p
         # If there are no search_items_queue either the loop has finished (or been
         # cleared above) -- either way, time to fill it back up
         if search_items_queue.empty():
-            log.debug('Search queue empty, restarting loop')
+            log.info('Search queue empty, restarting scanners after %d second wave delay', args.wave_delay)
+            time.sleep(args.wave_delay)
             for step, step_location in enumerate(generate_location_steps(current_location, args.step_limit), 1):
                 log.debug('Queueing step %d @ %f/%f/%f', step, step_location[0], step_location[1], step_location[2])
                 search_args = (step, step_location)
@@ -173,7 +174,7 @@ def search_overseer_thread(args, new_location_queue, pause_bit, encryption_lib_p
         #     log.info('Search queue processing, %d items left', search_items_queue.qsize())
 
         # Now we just give a little pause here
-        time.sleep(args.wave_delay)
+        time.sleep(1)
 
 
 def search_worker_thread(args, account, search_items_queue, parse_lock, encryption_lib_path):
