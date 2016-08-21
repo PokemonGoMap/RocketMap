00a0bfd71678420dbbbcf58271ba87f733cb967b
Mystery-Pokemon-Man <moussa.uk+mysterypokemonman@gmail.com>
2016-08-21 18:46:21 +0100

    Remove whitespace

--- pogom/models.py
+++ pogom/models.py
@@ -553,7 +553,7 @@ def clean_db_loop(args):
                      .where((ScannedLocation.last_modified <
                             (datetime.utcnow() - timedelta(minutes=30)))))
             query.execute()
-            
+
             # Remove active modifier from expired lured pokestops
             query = (Pokestop
                      .update(lure_expiration=None)
