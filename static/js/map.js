01cc8c59eb44b14f992073feb93220d3846e2ecf
BuildTools <Japu_D_Cret@web.de>
2016-08-29 20:28:05 +0200

    added higher search delay

--- static/js/map.js
+++ static/js/map.js
@@ -2122,7 +2122,7 @@ $(function () {
 
   // run interval timers to regularly update map and timediffs
   window.setInterval(updateLabelDiffTime, 1000)
-  window.setInterval(updateMap, 5000)
+  window.setInterval(updateMap, 10000)
   window.setInterval(function () {
     if (navigator.geolocation && Store.get('geoLocate')) {
       navigator.geolocation.getCurrentPosition(function (position) {
