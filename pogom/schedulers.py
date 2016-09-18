5c851f6afe260296f311b41ee01b9e023623a7f7
Wei Dai <x@wei23.net>
2016-09-17 22:53:02 -0700

    Remove redundant information

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -556,8 +556,6 @@ class SpawnScanSpeedLimit(BaseScheduler):
             log.info('Number of scan delays: %d.' % len(delays))
             log.info('Average delay: %f seconds.' % (sum(delays) / len(delays)))
             log.info('Max delay: %f seconds.' % max(delays))
-            if max(delays) > self.args.max_delay:
-                log.info('Cannot assign spawn points under max-delay. Try increasing number of accounts, decreasing number of spawn points, or increasing max-delay.')
         else:
             log.info('No additional delay is added to any spawn point.')
 
