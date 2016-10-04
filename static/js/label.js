6fbb3810fab3dcada8c535647330cdc2514f8016
venimo <info@venimo.nl>
2016-10-04 04:25:01 +0200

    I wanna sleep Travis :(

--- static/js/label.js
+++ static/js/label.js
@@ -17,12 +17,12 @@ inherits(MarkerLabel_, google.maps.OverlayView);
 MarkerLabel_.getSharedCross = function (crossURL) {
   var div;
   if (typeof MarkerLabel_.getSharedCross.crossDiv === "undefined") {
-      div = document.createElement("img");
-      div.style.cssText = "position: absolute; z-index: 1000002; display: none;";
-      div.style.marginLeft = "-8px";
-      div.style.marginTop = "-9px";
-      div.src = crossURL;
-      MarkerLabel_.getSharedCross.crossDiv = div;
+    div = document.createElement("img");
+    div.style.cssText = "position: absolute; z-index: 1000002; display: none;";
+    div.style.marginLeft = "-8px";
+    div.style.marginTop = "-9px";
+    div.src = crossURL;
+    MarkerLabel_.getSharedCross.crossDiv = div;
   }
   return MarkerLabel_.getSharedCross.crossDiv;
 };
@@ -39,161 +39,161 @@ MarkerLabel_.prototype.onAdd = function () {
   var cRaiseOffset = 20;
   var cDraggingCursor = "url(" + this.handCursorURL_ + ")";
   var cAbortEvent = function (e) {
-      if (e.preventDefault) {
-          e.preventDefault();
-      }
-      e.cancelBubble = true;
-      if (e.stopPropagation) {
-          e.stopPropagation();
-      }
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.cancelBubble = true;
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
   };
   var cStopBounce = function () {
-      me.marker_.setAnimation(null);
+    me.marker_.setAnimation(null);
   };
   this.getPanes().markerLayer.appendChild(this.labelDiv_);
   if (typeof MarkerLabel_.getSharedCross.processed === "undefined") {
-      this.getPanes().markerLayer.appendChild(this.crossDiv_);
-      MarkerLabel_.getSharedCross.processed = true;
+    this.getPanes().markerLayer.appendChild(this.crossDiv_);
+    MarkerLabel_.getSharedCross.processed = true;
   }
   this.listeners_ = [google.maps.event.addDomListener(this.labelDiv_, "mouseover", function (e) {
-      if (me.marker_.getDraggable() || me.marker_.getClickable()) {
-          this.style.cursor = "pointer";
-          google.maps.event.trigger(me.marker_, "mouseover", e);
-      }
+    if (me.marker_.getDraggable() || me.marker_.getClickable()) {
+      this.style.cursor = "pointer";
+      google.maps.event.trigger(me.marker_, "mouseover", e);
+    }
   }), google.maps.event.addDomListener(this.labelDiv_, "mouseout", function (e) {
-      if ((me.marker_.getDraggable() || me.marker_.getClickable()) && !cDraggingLabel) {
-          this.style.cursor = me.marker_.getCursor();
-          google.maps.event.trigger(me.marker_, "mouseout", e);
-      }
+    if ((me.marker_.getDraggable() || me.marker_.getClickable()) && !cDraggingLabel) {
+      this.style.cursor = me.marker_.getCursor();
+      google.maps.event.trigger(me.marker_, "mouseout", e);
+    }
   }), google.maps.event.addDomListener(this.labelDiv_, "mousedown", function (e) {
-      cDraggingLabel = false;
-      if (me.marker_.getDraggable()) {
-          cMouseIsDown = true;
-          this.style.cursor = cDraggingCursor;
-      }
-      if (me.marker_.getDraggable() || me.marker_.getClickable()) {
-          google.maps.event.trigger(me.marker_, "mousedown", e);
-          cAbortEvent(e);
-      }
+    cDraggingLabel = false;
+    if (me.marker_.getDraggable()) {
+      cMouseIsDown = true;
+      this.style.cursor = cDraggingCursor;
+    }
+    if (me.marker_.getDraggable() || me.marker_.getClickable()) {
+      google.maps.event.trigger(me.marker_, "mousedown", e);
+      cAbortEvent(e);
+    }
   }), google.maps.event.addDomListener(document, "mouseup", function (mEvent) {
-      var position;
-      if (cMouseIsDown) {
-          cMouseIsDown = false;
-          me.eventDiv_.style.cursor = "pointer";
-          google.maps.event.trigger(me.marker_, "mouseup", mEvent);
+    var position;
+    if (cMouseIsDown) {
+      cMouseIsDown = false;
+      me.eventDiv_.style.cursor = "pointer";
+      google.maps.event.trigger(me.marker_, "mouseup", mEvent);
+    }
+    if (cDraggingLabel) {
+      if (cRaiseEnabled) {
+        position = me.getProjection().fromLatLngToDivPixel(me.marker_.getPosition());
+        position.y += cRaiseOffset;
+        me.marker_.setPosition(me.getProjection().fromDivPixelToLatLng(position));
+        try {
+          me.marker_.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(cStopBounce, 1406);
+        } catch (e) {}
       }
-      if (cDraggingLabel) {
-          if (cRaiseEnabled) {
-              position = me.getProjection().fromLatLngToDivPixel(me.marker_.getPosition());
-              position.y += cRaiseOffset;
-              me.marker_.setPosition(me.getProjection().fromDivPixelToLatLng(position));
-              try {
-                  me.marker_.setAnimation(google.maps.Animation.BOUNCE);
-                  setTimeout(cStopBounce, 1406);
-              } catch (e) {}
-          }
-          me.crossDiv_.style.display = "none";
-          me.marker_.setZIndex(cSavedZIndex);
-          cIgnoreClick = true;
-          cDraggingLabel = false;
-          mEvent.latLng = me.marker_.getPosition();
-          google.maps.event.trigger(me.marker_, "dragend", mEvent);
-      }
+      me.crossDiv_.style.display = "none";
+      me.marker_.setZIndex(cSavedZIndex);
+      cIgnoreClick = true;
+      cDraggingLabel = false;
+      mEvent.latLng = me.marker_.getPosition();
+      google.maps.event.trigger(me.marker_, "dragend", mEvent);
+    }
   }), google.maps.event.addListener(me.marker_.getMap(), "mousemove", function (mEvent) {
-      var position;
-      if (cMouseIsDown) {
-          if (cDraggingLabel) {
-              mEvent.latLng = new google.maps.LatLng(mEvent.latLng.lat() - cLatOffset, mEvent.latLng.lng() - cLngOffset);
-              position = me.getProjection().fromLatLngToDivPixel(mEvent.latLng);
-              if (cRaiseEnabled) {
-                  me.crossDiv_.style.left = position.x + "px";
-                  me.crossDiv_.style.top = position.y + "px";
-                  me.crossDiv_.style.display = "";
-                  position.y -= cRaiseOffset;
-              }
-              me.marker_.setPosition(me.getProjection().fromDivPixelToLatLng(position));
-              if (cRaiseEnabled) {
-                  me.eventDiv_.style.top = (position.y + cRaiseOffset) + "px";
-              }
-              google.maps.event.trigger(me.marker_, "drag", mEvent);
-          } else {
-              cLatOffset = mEvent.latLng.lat() - me.marker_.getPosition().lat();
-              cLngOffset = mEvent.latLng.lng() - me.marker_.getPosition().lng();
-              cSavedZIndex = me.marker_.getZIndex();
-              cStartPosition = me.marker_.getPosition();
-              cStartCenter = me.marker_.getMap().getCenter();
-              cRaiseEnabled = me.marker_.get("raiseOnDrag");
-              cDraggingLabel = true;
-              me.marker_.setZIndex(1000000);
-              mEvent.latLng = me.marker_.getPosition();
-              google.maps.event.trigger(me.marker_, "dragstart", mEvent);
-          }
-      }
-  }), google.maps.event.addDomListener(document, "keydown", function (e) {
+    var position;
+    if (cMouseIsDown) {
       if (cDraggingLabel) {
-          if (e.keyCode === 27) {
-              cRaiseEnabled = false;
-              me.marker_.setPosition(cStartPosition);
-              me.marker_.getMap().setCenter(cStartCenter);
-              google.maps.event.trigger(document, "mouseup", e);
-          }
+        mEvent.latLng = new google.maps.LatLng(mEvent.latLng.lat() - cLatOffset, mEvent.latLng.lng() - cLngOffset);
+        position = me.getProjection().fromLatLngToDivPixel(mEvent.latLng);
+        if (cRaiseEnabled) {
+          me.crossDiv_.style.left = position.x + "px";
+          me.crossDiv_.style.top = position.y + "px";
+          me.crossDiv_.style.display = "";
+          position.y -= cRaiseOffset;
+        }
+        me.marker_.setPosition(me.getProjection().fromDivPixelToLatLng(position));
+        if (cRaiseEnabled) {
+          me.eventDiv_.style.top = (position.y + cRaiseOffset) + "px";
+        }
+        google.maps.event.trigger(me.marker_, "drag", mEvent);
+      } else {
+        cLatOffset = mEvent.latLng.lat() - me.marker_.getPosition().lat();
+        cLngOffset = mEvent.latLng.lng() - me.marker_.getPosition().lng();
+        cSavedZIndex = me.marker_.getZIndex();
+        cStartPosition = me.marker_.getPosition();
+        cStartCenter = me.marker_.getMap().getCenter();
+        cRaiseEnabled = me.marker_.get("raiseOnDrag");
+        cDraggingLabel = true;
+        me.marker_.setZIndex(1000000);
+        mEvent.latLng = me.marker_.getPosition();
+        google.maps.event.trigger(me.marker_, "dragstart", mEvent);
       }
+    }
+  }), google.maps.event.addDomListener(document, "keydown", function (e) {
+    if (cDraggingLabel) {
+      if (e.keyCode === 27) {
+        cRaiseEnabled = false;
+        me.marker_.setPosition(cStartPosition);
+        me.marker_.getMap().setCenter(cStartCenter);
+        google.maps.event.trigger(document, "mouseup", e);
+      }
+    }
   }), google.maps.event.addDomListener(this.labelDiv_, "click", function (e) {
-      if (me.marker_.getDraggable() || me.marker_.getClickable()) {
-          if (cIgnoreClick) {
-              cIgnoreClick = false;
-          } else {
-              google.maps.event.trigger(me.marker_, "click", e);
-              cAbortEvent(e);
-          }
+    if (me.marker_.getDraggable() || me.marker_.getClickable()) {
+      if (cIgnoreClick) {
+        cIgnoreClick = false;
+      } else {
+        google.maps.event.trigger(me.marker_, "click", e);
+        cAbortEvent(e);
       }
+    }
   }), google.maps.event.addDomListener(this.labelDiv_, "dblclick", function (e) {
-      if (me.marker_.getDraggable() || me.marker_.getClickable()) {
-          google.maps.event.trigger(me.marker_, "dblclick", e);
-          cAbortEvent(e);
-      }
+    if (me.marker_.getDraggable() || me.marker_.getClickable()) {
+      google.maps.event.trigger(me.marker_, "dblclick", e);
+      cAbortEvent(e);
+    }
   }), google.maps.event.addListener(this.marker_, "dragstart", function (mEvent) {
-      if (!cDraggingLabel) {
-          cRaiseEnabled = this.get("raiseOnDrag");
-      }
+    if (!cDraggingLabel) {
+      cRaiseEnabled = this.get("raiseOnDrag");
+    }
   }), google.maps.event.addListener(this.marker_, "drag", function (mEvent) {
-      if (!cDraggingLabel) {
-          if (cRaiseEnabled) {
-              me.setPosition(cRaiseOffset);
-              me.labelDiv_.style.zIndex = 1000000 + (this.get("labelInBackground") ? -1 : +1);
-          }
+    if (!cDraggingLabel) {
+      if (cRaiseEnabled) {
+        me.setPosition(cRaiseOffset);
+        me.labelDiv_.style.zIndex = 1000000 + (this.get("labelInBackground") ? -1 : +1);
       }
+    }
   }), google.maps.event.addListener(this.marker_, "dragend", function (mEvent) {
-      if (!cDraggingLabel) {
-          if (cRaiseEnabled) {
-              me.setPosition(0);
-          }
+    if (!cDraggingLabel) {
+      if (cRaiseEnabled) {
+        me.setPosition(0);
       }
+    }
   }), google.maps.event.addListener(this.marker_, "position_changed", function () {
-      me.setPosition();
+    me.setPosition();
   }), google.maps.event.addListener(this.marker_, "zindex_changed", function () {
-      me.setZIndex();
+    me.setZIndex();
   }), google.maps.event.addListener(this.marker_, "visible_changed", function () {
-      me.setVisible();
+    me.setVisible();
   }), google.maps.event.addListener(this.marker_, "labelvisible_changed", function () {
-      me.setVisible();
+    me.setVisible();
   }), google.maps.event.addListener(this.marker_, "title_changed", function () {
-      me.setTitle();
+    me.setTitle();
   }), google.maps.event.addListener(this.marker_, "labelcontent_changed", function () {
-      me.setContent();
+    me.setContent();
   }), google.maps.event.addListener(this.marker_, "labelanchor_changed", function () {
-      me.setAnchor();
+    me.setAnchor();
   }), google.maps.event.addListener(this.marker_, "labelclass_changed", function () {
-      me.setStyles();
+    me.setStyles();
   }), google.maps.event.addListener(this.marker_, "labelstyle_changed", function () {
-      me.setStyles();
+    me.setStyles();
   })];
 };
 MarkerLabel_.prototype.onRemove = function () {
   var i;
   this.labelDiv_.parentNode.removeChild(this.labelDiv_);
   for (i = 0; i < this.listeners_.length; i++) {
-      google.maps.event.removeListener(this.listeners_[i]);
+    google.maps.event.removeListener(this.listeners_[i]);
   }
 };
 MarkerLabel_.prototype.draw = function () {
@@ -204,10 +204,10 @@ MarkerLabel_.prototype.draw = function () {
 MarkerLabel_.prototype.setContent = function () {
   var content = this.marker_.get("labelContent");
   if (typeof content.nodeType === "undefined") {
-      this.labelDiv_.innerHTML = content;
+    this.labelDiv_.innerHTML = content;
   } else {
-      this.labelDiv_.innerHTML = "";
-      this.labelDiv_.appendChild(content);
+    this.labelDiv_.innerHTML = "";
+    this.labelDiv_.appendChild(content);
   }
 };
 MarkerLabel_.prototype.setTitle = function () {
@@ -219,9 +219,9 @@ MarkerLabel_.prototype.setStyles = function () {
   this.labelDiv_.style.cssText = "";
   labelStyle = this.marker_.get("labelStyle");
   for (i in labelStyle) {
-      if (labelStyle.hasOwnProperty(i)) {
-          this.labelDiv_.style[i] = labelStyle[i];
-      }
+    if (labelStyle.hasOwnProperty(i)) {
+      this.labelDiv_.style[i] = labelStyle[i];
+    }
   }
   this.setMandatoryStyles();
 };
@@ -229,8 +229,8 @@ MarkerLabel_.prototype.setMandatoryStyles = function () {
   this.labelDiv_.style.position = "absolute";
   this.labelDiv_.style.overflow = "hidden";
   if (typeof this.labelDiv_.style.opacity !== "undefined" && this.labelDiv_.style.opacity !== "") {
-      this.labelDiv_.style.MsFilter = "\"progid:DXImageTransform.Microsoft.Alpha(opacity=" + (this.labelDiv_.style.opacity * 100) + ")\"";
-      this.labelDiv_.style.filter = "alpha(opacity=" + (this.labelDiv_.style.opacity * 100) + ")";
+    this.labelDiv_.style.MsFilter = "\"progid:DXImageTransform.Microsoft.Alpha(opacity=" + (this.labelDiv_.style.opacity * 100) + ")\"";
+    this.labelDiv_.style.filter = "alpha(opacity=" + (this.labelDiv_.style.opacity * 100) + ")";
   }
   this.setAnchor();
   this.setPosition();
@@ -244,7 +244,7 @@ MarkerLabel_.prototype.setAnchor = function () {
 MarkerLabel_.prototype.setPosition = function (yOffset) {
   var position = this.getProjection().fromLatLngToDivPixel(this.marker_.getPosition());
   if (typeof yOffset === "undefined") {
-      yOffset = 0;
+    yOffset = 0;
   }
   this.labelDiv_.style.left = Math.round(position.x) + "px";
   this.labelDiv_.style.top = Math.round(position.y - yOffset) + "px";
@@ -253,16 +253,16 @@ MarkerLabel_.prototype.setPosition = function (yOffset) {
 MarkerLabel_.prototype.setZIndex = function () {
   var zAdjust = (this.marker_.get("labelInBackground") ? -1 : +1);
   if (typeof this.marker_.getZIndex() === "undefined") {
-      this.labelDiv_.style.zIndex = parseInt(this.labelDiv_.style.top, 10) + zAdjust;
+    this.labelDiv_.style.zIndex = parseInt(this.labelDiv_.style.top, 10) + zAdjust;
   } else {
-      this.labelDiv_.style.zIndex = this.marker_.getZIndex() + zAdjust;
+    this.labelDiv_.style.zIndex = this.marker_.getZIndex() + zAdjust;
   }
 };
 MarkerLabel_.prototype.setVisible = function () {
   if (this.marker_.get("labelVisible")) {
-      this.labelDiv_.style.display = this.marker_.getVisible() ? "block" : "none";
+    this.labelDiv_.style.display = this.marker_.getVisible() ? "block" : "none";
   } else {
-      this.labelDiv_.style.display = "none";
+    this.labelDiv_.style.display = "none";
   }
 };
 
@@ -274,19 +274,19 @@ function MarkerWithLabel (opt_options) {
   opt_options.labelStyle = opt_options.labelStyle || {};
   opt_options.labelInBackground = opt_options.labelInBackground || false;
   if (typeof opt_options.labelVisible === "undefined") {
-      opt_options.labelVisible = true;
+    opt_options.labelVisible = true;
   }
   if (typeof opt_options.raiseOnDrag === "undefined") {
-      opt_options.raiseOnDrag = true;
+    opt_options.raiseOnDrag = true;
   }
   if (typeof opt_options.clickable === "undefined") {
-      opt_options.clickable = true;
+    opt_options.clickable = true;
   }
   if (typeof opt_options.draggable === "undefined") {
-      opt_options.draggable = false;
+    opt_options.draggable = false;
   }
   if (typeof opt_options.optimized === "undefined") {
-      opt_options.optimized = false;
+    opt_options.optimized = false;
   }
   opt_options.crossImage = opt_options.crossImage || "http" + (document.location.protocol === "https:" ? "s" : "") + "://maps.gstatic.com/intl/en_us/mapfiles/drag_cross_67_16.png";
   opt_options.handCursor = opt_options.handCursor || "http" + (document.location.protocol === "https:" ? "s" : "") + "://maps.gstatic.com/intl/en_us/mapfiles/closedhand_8_8.cur";
