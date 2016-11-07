6afdd56c809eca027a13a1f75187bd4d1bd5474b
averylarry <temp3@kumsher.com>
2016-11-07 07:40:11 -0600

    Update search.py
    Forgot half the code.

--- pogom/search.py
+++ pogom/search.py
@@ -487,6 +487,18 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                     # No sleep here; we've not done anything worth sleeping for. Plus we clearly need to catch up!
                     continue
 
+                # too fast?
+                if extra_delay:
+                    status['message'] = 'Too fast for {:6f},{:6f}; waiting {}s...'.format(step_location[0], step_location[1], extra_delay)
+                    log.info(status['message'])
+                    continue_time = time.time() + extra_delay
+
+                    while time.time() < continue_time:
+                        time.sleep(1)
+                        remain = int(continue_time - time.time())
+                        if remain:
+                            status['message'] = 'Too fast for {:6f},{:6f}; waiting {}s...'.format(step_location[0], step_location[1], remain)
+
                 # Let the api know where we intend to be for this loop
                 # doing this before check_login so it does not also have to be done there
                 # when the auth token is refreshed
