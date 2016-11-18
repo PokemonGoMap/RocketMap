0a7ff43a23b6e28bf161d0fb867ea9d77df211ee
Paraselene Alex Fung <paraselene@gmail.com>
2016-11-18 10:49:28 +0800

    fix some mistake

--- pogom/get_token.py
+++
@@ -1,6 +0,0 @@
-
-        self.route("/get_token", methods=['GET'])(self.get_token)
-
-    def get_token(self):
-        token = Token.get_match(datetime.utcnow()-timedelta(minutes=30))
-        return token.token
