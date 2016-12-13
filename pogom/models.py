92e7ac0777cf76ec597eedf77ace921d121048e4
Artifice <artifice@c-loomis.com>
2016-12-14 07:09:52 +0900

    Speed scan v2 (#1617)
    * Fix for statistics update not showing
    
    * Add more workers/beehive
    
    Add —workers-per-hive (-wph) arg, default 1
    Allow more than one worker per beehive
    
    * Index error fix
    
    * Reduce distance calc time
    
    Change from vincenty to equirectangular approximation distance to
    reduce calc time by 20X
    
    * Change from vincenty to equirectangular approximation
    
    Change from vincenty to equirectangular approximation to reduce CPU load
    
    * Flake8
    
    * Fix for large -st waiting for overseer refresh
    
    * Fixes
    
    Fix for spawn kind hhhs unseen errors
    Fix for speed violations due to slow proxy
    Fix for moving 0m display bug
    
    * Documentation v2
    
    * Reduce calcs for locations with -bh
    
    * Minor text fix
    
    * Fix for fix for number of leaps with -bh
    
    * Update commandline.md
    
    Plus minor text fixes
    
    rebase

--- pogom/models.py
+++ pogom/models.py
@@ -11,7 +11,9 @@ import time
 import geopy
 import math
 
-from peewee import SqliteDatabase, InsertQuery, Check, CompositeKey, \
+from peewee import SqliteDatabase, InsertQuery, \
+    Check, CompositeKey, \
+
     IntegerField, CharField, DoubleField, BooleanField, \
     DateTimeField, fn, DeleteQuery, FloatField, SQL, TextField, JOIN
 
@@ -1083,7 +1085,8 @@ class SpawnPoint(BaseModel):
     def start_end(cls, sp, spawn_delay=0, links=False):
         links_arg = links
         links = links if links else str(sp['links'])
-
+        if links == '????':  # clean up for old data
+            links = str(sp['kind'].replace('s', '?'))
 
         # make some assumptions if link not fully identified
         if links.count('-') == 0:
