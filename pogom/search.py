12b9ab7215b90eeab59b4bf8ec713cb5dffdc029
Paraselene Alex Fung <paraselene@gmail.com>
2016-12-14 15:53:32 +0800

    Fix too late problem

--- pogom/search.py
+++ pogom/search.py
@@ -579,6 +579,7 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                     status['message'] = 'Too late for location {:6f},{:6f}; skipping'.format(step_location[0], step_location[1])
                     log.info(status['message'])
                     # No sleep here; we've not done anything worth sleeping for. Plus we clearly need to catch up!
+                    retry_failed_search_item = None
                     continue
 
                 # Let the api know where we intend to be for this loop.
