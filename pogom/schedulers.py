1619f0d85fa86978b6a2e998923559444346689b
chowderchow <andrewcxw@gmail.com>
2017-01-06 12:15:41 +0000

    Missing periods in comments

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -867,7 +867,7 @@ class SchedulerFactory():
         raise NotImplementedError("The requested scheduler has not been implemented")
 
 
-# The KeyScheduler returns a scheduler that cycles through the given hash server keys
+# The KeyScheduler returns a scheduler that cycles through the given hash server keys.
 class KeyScheduler(object):
     def __init__(self, keys):
         self.keys = keys
--- pogom/search.py
+++ pogom/search.py
@@ -305,7 +305,7 @@ def search_overseer_thread(args, new_location_queue, pause_bit, heartb, db_updat
         t.daemon = True
         t.start()
 
-    # Create the key scheduler
+    # Create the key scheduler.
     if args.hash_key:
         key_scheduler = schedulers.KeyScheduler(args.hash_key).scheduler()
 
@@ -602,7 +602,7 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                     api.activate_hash_server(key)
                     log.info('Using key {} for this scan.'.format(key))
 
-                # Ok, let's get started -- check our login status
+                # Ok, let's get started -- check our login status.
                 status['message'] = 'Logging in...'
                 check_login(args, account, api, step_location, status['proxy_url'])
 
