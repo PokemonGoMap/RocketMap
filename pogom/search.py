64c4974d3a718bee799ce10e61a601fbe868eca5
hokiepokedad <pogovaalerts@gmail.com>
2017-01-08 20:28:20 -0500

    Hash Key Status Status Page

--- pogom/search.py
+++ pogom/search.py
@@ -344,7 +344,7 @@ def search_overseer_thread(args, new_location_queue, pause_bit, heartb, db_updat
         'scheduler': args.scheduler,
         'scheduler_status': {'tth_found': 0}
     }
-
+    
     # Create the key scheduler.
     if args.hash_key:
         log.info('Enabling hashing key scheduler...')
@@ -805,7 +805,7 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                 api.set_position(*step_location)
 
                 if args.hash_key:
-                    key = key_scheduler.next()
+                    key = key_scheduler.next_key()
                     log.debug('Using key {} for this scan.'.format(key))
                     api.activate_hash_server(key)
 
