892afcaac39639a3295da4d2cd4b84c5d79c8fe9
houkop <hompfull@gmail.com>
2017-01-13 19:33:21 +0300

    timers

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -978,6 +978,14 @@ var StoreOptions = {
     default: 0,
     type: StoreTypes.Number
   },
+  'showTimers': {
+    default: true,
+    type: StoreTypes.Boolean
+  },
+  'hideTimersAtZoomLevel': {
+    default: 18,
+    type: StoreTypes.Number
+  },
   'searchMarkerStyle': {
     default: 'google',
     type: StoreTypes.String
@@ -1060,22 +1068,42 @@ function setupPokemonMarker (item, map, isBounceDisabled) {
   var pokemonIndex = item['pokemon_id'] - 1
   var sprite = pokemonSprites[Store.get('pokemonIcons')] || pokemonSprites['highres']
   var icon = getGoogleSprite(pokemonIndex, sprite, iconSize)
+  var hideTimersAtZoomLevel = Store.get('hideTimersAtZoomLevel')
+  var showTimers = Store.get('showTimers')
 
   var animationDisabled = false
   if (isBounceDisabled === true) {
     animationDisabled = true
   }
 
-  var marker = new google.maps.Marker({
-    position: {
-      lat: item['latitude'],
-      lng: item['longitude']
-    },
-    zIndex: 9999,
-    map: map,
-    icon: icon,
-    animationDisabled: animationDisabled
-  })
+  var marker
+
+  if (showTimers && map.getZoom() >= hideTimersAtZoomLevel) {
+    marker = new MarkerWithLabel({ // eslint-disable-line no-undef
+      position: {
+        lat: item['latitude'],
+        lng: item['longitude']
+      },
+      zIndex: 9999,
+      map: map,
+      icon: icon,
+      labelAnchor: new google.maps.Point(13, -iconSize / 2.4),
+      labelContent: '<span class=\'label-countdown\' disappears-at=\'' + item['disappear_time'] + '\'> </span>',
+      labelClass: 'pokemonlabel',
+      animationDisabled: animationDisabled
+    })
+  } else {
+    marker = new google.maps.Marker({
+      position: {
+        lat: item['latitude'],
+        lng: item['longitude']
+      },
+      zIndex: 9999,
+      map: map,
+      icon: icon,
+      animationDisabled: animationDisabled
+    })
+  }
 
   return marker
 }
