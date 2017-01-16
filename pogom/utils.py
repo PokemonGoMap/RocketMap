6a3ce63213cb97edc3a8ae8c9b84c3f23f82f455
tomballgithub <jeoff@krontz.com>
2017-01-16 15:57:44 -0600

    Fix travis

--- pogom/utils.py
+++ pogom/utils.py
@@ -233,7 +233,7 @@ def get_args():
     parser.add_argument('--ssl-privatekey',
                         help='Path to SSL private key file.')
     parser.add_argument('-ps', '--print-status',
-                        help='Show a status screen instead of log messages. Can switch between status and logs by pressing enter.  Optionally specify "logs" to startup in logging mode.',nargs='?', const='status', default=False, metavar='logs')
+                        help='Show a status screen instead of log messages. Can switch between status and logs by pressing enter.  Optionally specify "logs" to startup in logging mode.', nargs='?', const='status', default=False, metavar='logs')
     parser.add_argument('-slt', '--stats-log-timer',
                         help='In log view, list per hr stats every X seconds', type=int, default=0)
     parser.add_argument('-sn', '--status-name', default=None,
