fa68125658a8e86256eee7dae9d3efbf25a4afa3
HokiePokeDad <pogovaalerts@gmail.com>
2017-01-22 09:06:29 -0500

    Re-added timeout handler

--- static/js/map.js
+++ static/js/map.js
@@ -1029,6 +1029,19 @@ function loadRawData() {
                 rawDataIsLoading = true
             }
         },
+        error: function() {
+            if (!$timeoutDialog) {
+                var opts = {
+                    title: 'Reduce marker settings'
+                };
+
+                $timeoutDialog = $('<div>Hmm... we\'re having problems getting data for your criteria. Try reducing what you\'re showing and zooming in to limit what\'s returned.</div>').dialog(opts);
+                $timeoutDialog.dialog('open');
+            }
+            else if (!$timeoutDialog.dialog('isOpen')) {
+                $timeoutDialog.dialog('open');
+            }
+        },
         complete: function () {
             rawDataIsLoading = false
         }
