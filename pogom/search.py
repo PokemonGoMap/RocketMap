906cdb5f06eeff9f768011399fa4af8d8834acff
Arengo <PokemonGoMap@outlook.de>
2017-01-25 02:43:47 +0100

    One space looks fine in the status page

--- pogom/search.py
+++ pogom/search.py
@@ -1084,7 +1084,7 @@ def search_worker_thread(args, account_queue, account_failures,
                 # Delay the desired amount after "scan" completion.
                 delay = scheduler.delay(status['last_scan_date'])
 
-                status['message'] += '  Sleeping {}s until {}.'.format(
+                status['message'] += ' Sleeping {}s until {}.'.format(
                     delay,
                     time.strftime(
                         '%H:%M:%S',
