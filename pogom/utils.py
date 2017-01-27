ecfe0f4ded6ec897e1dc605c79b621625297128d
Sébastien Vercammen <contact@sebastienvercammen.be>
2017-01-27 20:24:01 +0100

    Changed dottedQuadToNum type.

--- pogom/utils.py
+++ pogom/utils.py
@@ -884,8 +884,7 @@ def complete_tutorial(api, account, tutorial_state):
 
 
 def dottedQuadToNum(ip):
-    "convert decimal dotted quad string to long integer"
-    return struct.unpack('L', socket.inet_aton(ip))[0]
+    return struct.unpack("!I", socket.inet_aton(ip))[0]
 
 
 def get_blacklist():
