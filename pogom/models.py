f318d0b6ae579475a5726c65dd095e5c49e04ec1
Arengo <PokemonGoMap@outlook.de>
2017-02-16 19:19:56 +0100

    Stupid Arengo was stupid

--- pogom/models.py
+++ pogom/models.py
@@ -1944,8 +1944,8 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                             log.debug('Lured Pokestop - Fort details found:')
                             log.debug(fort_details)
 
-                            for 'fort_id' & 'name' & 'image_urls' & \
-                                    'description' in fort_details:
+                            if ('fort_id' and 'name' and 'image_urls' and
+                                    'description' in fort_details):
                                 fort_id = fort_details['fort_id']
                                 name = fort_details['name']
                                 image_urls = fort_details['image_urls']
@@ -1964,8 +1964,10 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                             log.debug(modifiers)
 
                             for modifier in modifiers:
-                                for 'item_id' & 'expiration_timestamp_ms' & \
-                                        'deployer_player_codename' in modifier:
+                                if ('item_id' and
+                                        'expiration_timestamp_ms' and
+                                        'deployer_player_codename'
+                                        in modifier):
                                     item_id = modifier['item_id']
                                     expiration_time = (
                                         datetime.utcfromtimestamp(
@@ -1994,8 +1996,8 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                             lure_info = f.get('lure_info')
                             log.debug('Lured Pokestop - Additional lure info:')
                             log.debug(lure_info)
-                            if 'encounter_id' & 'active_pokemon_id' & \
-                                    'lure_expires_timestamp_ms'in lure_info:
+                            if ('encounter_id' and 'active_pokemon_id' and
+                                    'lure_expires_timestamp_ms'in lure_info):
                                 printPokemon(
                                     lure_info['pokemon_id'],
                                     f['latitude'],
