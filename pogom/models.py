589254e5fe0b30e580ce675618d68d20796676f1
Markus Hilger <obihoernchende@gmail.com>
2017-03-02 01:04:24 +0100

    Make travis happy

--- pogom/models.py
+++ pogom/models.py
@@ -2579,8 +2579,6 @@ def database_migrate(db, old_ver):
             migrator.add_index('gymmember', ('last_scanned',), False),
             migrator.add_index('gymmember', ('pokemon_uid',), False),
             migrator.add_index('gympokemon', ('trainer_name',), False),
-            # was missing in a previous migration
-            #migrator.add_index('pokestop', ('last_updated',), False),
             migrator.add_index('pokestop', ('active_fort_modifier',), False),
             migrator.add_index('spawnpointdetectiondata', ('spawnpoint_id',),
                                False),
