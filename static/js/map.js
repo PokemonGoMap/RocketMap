9a0b1730af56eff5e9cc7b6b67b1140cadc55e78
EagleWorks <panoworks@hotmail.com>
2017-03-23 01:45:22 -0400

    Fixes Issue 1941, negative seconds
    Spawn point details can currently show negative seconds.  These should
    be substracted from 60 to get the correct second count.
    Note: This only fixes a formatting issue, not any issues with regard to
    actual spawn times.  For discussion on the latter, see Issue #1944

--- static/js/map.js
+++ static/js/map.js
@@ -578,7 +578,7 @@ function pokestopLabel(expireTime, latitude, longitude) {
 function formatSpawnTime(seconds) {
     // the addition and modulo are required here because the db stores when a spawn disappears
     // the subtraction to get the appearance time will knock seconds under 0 if the spawn happens in the previous hour
-    return ('0' + Math.floor(((seconds + 3600) % 3600) / 60)).substr(-2) + ':' + ('0' + seconds % 60).substr(-2)
+    return ('0' + Math.floor(((seconds + 3600) % 3600) / 60)).substr(-2) + ':' + ('0' + (seconds >= 0 ? seconds % 60 : 60 + (seconds % 60))).substr(-2)
 }
 
 function spawnpointLabel(item) {
