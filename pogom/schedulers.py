0d124ed7665076adfe6b2aaa07ed4d52f45d0ab2
Alderon86 <christianbriest86@gmail.com>
2017-03-23 01:45:25 +0100

    git pls update accordingly

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -1132,10 +1132,9 @@ class KeyScheduler(object):
         self.key_cycle = itertools.cycle(keys)
         self.curr_key = ''
 
-        hashkeys = self.keys.copy()
         hashkeys_db = {}
         for key in hashkeys:
-            hashkeys_db[key] = hashkeys[key].copy()
+            hashkeys_db[key] = self.keys.copy()
             hashkeys_db[key].pop('remaining', None)
             hashkeys_db[key]['key'] = key
         db_updates_queue.put((HashKeys, hashkeys_db))
