9cb67a5aacfe24656b9055de29c5d98e35a543bd
Jason Gauthier <jagauthier@gmail.com>
2017-04-21 15:00:06 -0400

    merge conflict removed player level. I was a bad boy, and didn't see it.

--- pogom/models.py
+++ pogom/models.py
@@ -2005,7 +2005,8 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                         'verified': SpawnPoint.tth_found(sp),
                         'seconds_until_despawn': seconds_until_despawn,
                         'spawn_start': start_end[0],
-                        'spawn_end': start_end[1]
+                        'spawn_end': start_end[1],
+                        'player_level': level
                         })
                     wh_update_queue.put(('pokemon', wh_poke))
         # Helping out the GC.
