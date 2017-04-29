97c9e08c22be516076145f69b69a36091c69be4a
mode <tymosier@yahoo.com>
2017-04-29 18:54:18 -0500

    Minor Text Fixes

--- example.py
+++ example.py
@@ -2,14 +2,14 @@
 # for those that messaged me with "i'm on dev, example.py dont work"
 # thunderfox : can we put an example.py in 2.0 that just prints
 # "you're a fucking retard"
-
+# Blue fix stuff and thing
 # 1000th commit, celebrate example.py!
 
 # dont make pr to fix pls
 
-print("Please refer to documentation, there is no more example.py, " +
-      "only runserver.py")
-fuck = raw_input("Would yous till like to massage the devs that " +
+print("Please refer to documentation, example.py, is down right now." +
+      "Only runserver.py is working.")
+fuck = raw_input("Would you like to massage the devs that " +
                  "example.py no work?")
 if "needful" in fuck.lower():
     print("poon bang lure pls")
