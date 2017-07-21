073eb7b7008ada249ac1975bb585e4fbd6d8ca35
IcognitoUnown <hamiltonian.hs@outlook.com>
2017-07-21 12:43:45 +0200

    Initial commit.

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -934,6 +934,10 @@ var StoreOptions = {
         default: false,
         type: StoreTypes.Boolean
     },
+    'showGeofences': {
+        default: false,
+        type: StoreTypes.Boolean
+    },
     'playSound': {
         default: false,
         type: StoreTypes.Boolean
