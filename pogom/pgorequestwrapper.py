a465b081ae21617cf7b8b667ff3aa0b2beb6a7f6
Sébastien Vercammen <hello@sebastienvercammen.be>
2017-07-27 23:41:01 +0200

    Rotate proxy on retry + sleep on HashingQuotaExceededException.

--- pogom/pgorequestwrapper.py
+++ pogom/pgorequestwrapper.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
+import random
 import logging
 
 from .utils import get_args
+from .proxy import get_new_proxy
+
+from pgoapi.exceptions import HashingQuotaExceededException
 
 log = logging.getLogger(__name__)
 
@@ -33,14 +38,33 @@ class PGoRequestWrapper:
 
     def do_call(self, retries_left, *args, **kwargs):
         try:
-            log.debug('Sending API request. Retries left: %d.', retries_left)
+            log.debug('Sending API request w/ %d retries left.', retries_left)
             return self.request.call(*args, **kwargs)
+        except HashingQuotaExceededException:
+            # Sleep a minimum to free some RPM.
+            time.sleep(random.uniform(0.75, 1.5))
         except:
-            if retries_left > 0:
-                log.debug('API request failed. Retrying...')
-                return self.do_call(retries_left - 1, *args, **kwargs)
-            else:
-                raise
+            pass
+
+        # If we've reached here, an exception was raised and the function
+        # hasn't returned yet.
+
+        # Try again if we have retries left.
+        if retries_left > 0:
+            # Rotate proxy.
+            proxy_idx, proxy_url = get_new_proxy(get_args())
+
+            if proxy_url:
+                log.debug('Changed to proxy: %s.', proxy_url)
+                proxy_config = {'http': proxy_url, 'https': proxy_url}
+                parent = self.request.__parent__
+                parent.set_proxy(proxy_config)
+                parent._auth_provider.set_proxy(proxy_config)
+
+            log.debug('API request failed. Retrying...')
+            return self.do_call(retries_left - 1, *args, **kwargs)
+        else:
+            raise
 
     def call(self, *args, **kwargs):
         # Retry x times on failure.
