ffb45ac4086bacefc2444ef39d0a637be28229ff
Sébastien Vercammen <hello@sebastienvercammen.be>
2017-07-28 12:37:11 +0200

    Fix proxy rotation.

--- pogom/pgorequestwrapper.py
+++ pogom/pgorequestwrapper.py
@@ -17,10 +17,10 @@ class PGoRequestWrapper:
 
     def __init__(self, request):
         log.debug('Wrapped PGoApiRequest.')
-        args = get_args()
+        self.args = get_args()
 
         self.request = request
-        self.retries = args.api_retries
+        self.retries = self.args.api_retries
 
     def __getattr__(self, attr):
         orig_attr = getattr(self.request, attr)
@@ -50,16 +50,17 @@ class PGoRequestWrapper:
         # hasn't returned yet.
 
         # Try again if we have retries left.
-        if args.proxy and retries_left > 0:
-            # Rotate proxy.
-            proxy_idx, proxy_url = get_new_proxy(get_args())
+        if retries_left > 0:
+            if self.args.proxy:
+                # Rotate proxy.
+                proxy_idx, proxy_url = get_new_proxy(get_args())
 
-            if proxy_url:
-                log.debug('Changed to proxy: %s.', proxy_url)
-                proxy_config = {'http': proxy_url, 'https': proxy_url}
-                parent = self.request.__parent__
-                parent.set_proxy(proxy_config)
-                parent._auth_provider.set_proxy(proxy_config)
+                if proxy_url:
+                    log.debug('Changed to proxy: %s.', proxy_url)
+                    proxy_config = {'http': proxy_url, 'https': proxy_url}
+                    parent = self.request.__parent__
+                    parent.set_proxy(proxy_config)
+                    parent._auth_provider.set_proxy(proxy_config)
 
             log.debug('API request failed. Retrying...')
             return self.do_call(retries_left - 1, *args, **kwargs)
