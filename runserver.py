42cd0b49478d1cc5b61c4d8f953e2b2c1eebe336
neskk <neskk@oninet.pt>
2017-08-08 07:40:11 +0100

    Status name regex simplified and log message updated.

--- runserver.py
+++ runserver.py
@@ -195,9 +195,9 @@ def main():
     args = get_args()
 
     # Abort if status name is not valid.
-    regexp = re.compile('^([a-zA-Z0-9\s\-\_\.]+)$')
+    regexp = re.compile('^([\w\s\-\.]+)$')
     if not regexp.match(args.status_name):
-        log.critical('Status name must be alphanumeric.')
+        log.critical('Status name contains ilegal characters.')
         sys.exit(1)
 
     set_log_and_verbosity(log)
