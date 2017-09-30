ff737828b6ba2eee2d94037d9094d5a6835f94e2
Sébastien Vercammen <hello@sebastienvercammen.be>
2017-09-30 19:41:37 +0200

    Retry when stats request fails.

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -96,6 +96,27 @@ function updateStats() {
                     {'orderable': false, 'targets': [0, 7]}
                 ]
             })
+    }).fail(function () {
+        // Display error toast
+        toastr['error']('Request failed while getting data. Retrying...', 'Error getting data')
+        toastr.options = {
+            'closeButton': true,
+            'debug': false,
+            'newestOnTop': true,
+            'progressBar': false,
+            'positionClass': 'toast-top-right',
+            'preventDuplicates': true,
+            'onclick': null,
+            'showDuration': '300',
+            'hideDuration': '1000',
+            'timeOut': '25000',
+            'extendedTimeOut': '1000',
+            'showEasing': 'swing',
+            'hideEasing': 'linear',
+            'showMethod': 'fadeIn',
+            'hideMethod': 'fadeOut'
+        }
+        updateStats()
     })
 }
 
