8387ea13a228396250a41e14d71a7d3c7bc41943
Ashutosh <ashutoshmishra07@gmail.com>
2017-12-27 00:57:27 +0100

    Added css classes for the gym name color and motivation color

--- static/js/map.js
+++ static/js/map.js
@@ -644,7 +644,6 @@ function gymLabel(gym, includeMembers = true) {
     const lastScannedStr = getDateStr(gym.last_scanned)
     const lastModifiedStr = getDateStr(gym.last_modified)
     const slotsString = gym.slots_available ? (gym.slots_available === 1 ? '1 Free Slot' : `${gym.slots_available} Free Slots`) : 'No Free Slots'
-    const teamColor = ['85,85,85,1', '0,134,255,1', '255,26,26,1', '255,159,25,1']
     const teamName = gymTypes[gym.team_id]
     const isUpcomingRaid = raid != null && Date.now() < raid.start
     const isRaidStarted = isOngoingRaid(raid)
@@ -659,8 +658,8 @@ function gymLabel(gym, includeMembers = true) {
     const gymPoints = gym.total_cp
     const titleText = gym.name ? gym.name : (gym.team_id === 0 ? teamName : 'Team ' + teamName)
     const title = `
-      <div class='gym name' style='color:rgba(${teamColor[gym.team_id]})'>
-        ${titleText}
+      <div class='gym name'>
+        <span class='team ${gymTypes[gym.team_id].toLowerCase()}'>${titleText}</span>
       </div>`
 
     if (gym.team_id !== 0) {
@@ -2192,7 +2191,7 @@ function showGymDetails(id) { // eslint-disable-line no-unused-vars
 function getSidebarGymMember(pokemon) {
     var perfectPercent = getIv(pokemon.iv_attack, pokemon.iv_defense, pokemon.iv_stamina)
     var moveEnergy = Math.round(100 / pokemon.move_2_energy)
-    const motivationColor = ['16,155,49,1', '235,193,26,1', '255,25,25,1']
+    const motivationZone = ['Good', 'Average', 'Bad']
     const motivationPercentage = (pokemon.cp_decayed / pokemon.pokemon_cp) * 100
     var colorIdx = 0
     if (motivationPercentage <= 46.66) {
@@ -2209,7 +2208,7 @@ function getSidebarGymMember(pokemon) {
                         <td>
                             <div class="gym pokemon" style="line-height:1em;"><span class="gym pokemon name">${pokemon.pokemon_name}</span></div>
                             <div>
-                            <span class="gym pokemon motivation decayed" style='color:rgba(${motivationColor[colorIdx]})'>${pokemon.cp_decayed}</span>
+                            <span class="gym pokemon motivation decayed zone ${motivationZone[colorIdx].toLowerCase()}">${pokemon.cp_decayed}</span>
                             </div>
                             <div>
                             <span class="gym pokemon motivation cp">Max: ${pokemon.pokemon_cp}</span>
@@ -2250,11 +2249,6 @@ function getSidebarGymMember(pokemon) {
                                     <div class="type">PERFECT</div>
                                     <div class="value">
                                         ${perfectPercent.toFixed(0)}<span style="font-size: .6em;">%</span>
-                                    </div>
-                                </div>
-								<div class="type">CP</div>
-                                    <div class="value">
-                                        ${pokemon.pokemon_cp}
                                     </div>
                                 </div>
                             </div>
